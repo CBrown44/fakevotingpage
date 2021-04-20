0a215cedfb18f8e3745b2be48bbea5dcf0bec6f2
Caleb <cgbrown44@gmail.com>
2021-04-19 18:21:09 -0700

    Started on the javascript part andded my pics and fixed some styling

---
+++ java.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', (e) =>{
+    alert("You're Bad LOL!");
+});
+
+function voteHere() {
+    alert("Welcome to The 1990 something election");
+}
+function myFunction() {
+    alert("Bro, You sure?");
+    alert ('Positivly sure?');
+    alert ('Bro, you for real?');
+    alert('Come on bro, just vote Nixon');
+}
+function bigHead(x) {
+    x.style.height = "80px";
+    x.style.width = "80px";
+}
+function smollHead(x) {
+x.style.height = "10px";
+x.style.width = "10px";
+}
